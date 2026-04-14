88843f46ffb308cac06db93acc8912462ea5558e
Nalle Rooth <nalle.rooth@gmail.com>
2026-04-14 23:49:33 +0200

    removed some old files

--- src/main.c
+++ src/main.c
@@ -3,9 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "common.h"
-#include "file.h"
-#include "parse.h"
 #include <kv.h>
 
 void print_usage(char *argv[]) {
@@ -21,67 +18,4 @@ int main(int argc, char *argv[]) {
   printf("%ld\n", table->capacity);
 
   return 0;
-  int c;
-  bool newfile = false;
-  char *filepath = NULL;
-
-  int dbfd = -1;
-
-  struct dbheader_t *dbheader = NULL;
-
-  while ((c = getopt(argc, argv, "nf:")) != -1) {
-    switch (c) {
-    case 'n':
-      newfile = true;
-      break;
-    case 'f':
-      filepath = optarg;
-      break;
-    case '?':
-      printf("Unknown option -%c\n", c);
-      break;
-
-    default:
-      return -1;
-    }
-  }
-
-  if (filepath == NULL) {
-    fprintf(stderr, "Filepath is required\n");
-    print_usage(argv);
-
-    return -1;
-  }
-
-  if (newfile) {
-    dbfd = create_db_file(filepath);
-    if (dbfd == STATUS_ERROR) {
-      printf("Unable to create new file\n");
-      return -1;
-    }
-
-    if (create_db_header(&dbheader) == STATUS_ERROR) {
-      printf("Unable to create db header\n");
-      return -1;
-    }
-
-    if (output_file(dbfd, dbheader, NULL) == STATUS_ERROR) {
-      printf("Unable to write db header to file\n");
-      return -1;
-    }
-  } else {
-    dbfd = open_db_file(filepath);
-    if (dbfd == STATUS_ERROR) {
-      printf("Unable to open file\n");
-      return -1;
-    }
-
-    if (validate_db_header(dbfd, &dbheader) == STATUS_ERROR) {
-      printf("Invalid db header\n");
-      return -1;
-    }
-  }
-
-  printf("Newfile: %d\n", newfile);
-  printf("Filepath: %s\n", filepath);
 }
